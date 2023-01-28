cfe72f8887e19459d159cd60945e3986e696f680
manolisgi <114882721+manolisgi@users.noreply.github.com>
2023-01-28 20:51:11 +0000

    added a variable for the first fetch to be on the city at .weather-search

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+let citiesList = document.querySelector(".list-group");
 
 
 //add an event listener with event delegation for every button buttonsDiv 
 let buttonsDiv = document.querySelector(".cityButtons")
+
 buttonsDiv.addEventListener("click", event => {
     event.preventDefault();
+    let city = document.querySelector(".weather-search").value;
     if (event.target.tagName === 'BUTTON') {
+        console.log(city);
 
-
-
-
-
-        fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=cba5837e4c2716904395e63c014ee26b")
+        fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=cba5837e4c2716904395e63c014ee26b")
             .then(response => response.json())
             .then(citiesFound => {
                 let firstCity = citiesFound[0];
@@ -29,9 +29,27 @@ buttonsDiv.addEventListener("click", event => {
 
             })
 
-
-
-
     }
 
 });
+
+// function renderCities() {
+//     // Clear element and update todoCountSpan
+//     citiesList.innerHTML = "";
+  
+//     // Render a new li for each city
+//     for (var i = 0; i < citiesList.length; i++) {
+//       // create a variable that stores the "current" todo
+//       var city = citiesList[i];
+  
+//       // create a li element (list item) that we will append to our list later
+//       var li = document.createElement("li"); // <li></li>
+  
+//       // set the text of the list item to be the value of todo
+//       li.textContent = city;   // <li>Teach  HTML</li>
+  
+//       // make this the last child of the todoList, which is the <ul> on our page
+
+//       citiesList.appendChild(li);
+//     }
+//   }
