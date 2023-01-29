ebbef5ae15b3bf7006038a3f09f4e669b15fd881
manolisgi <114882721+manolisgi@users.noreply.github.com>
2023-01-29 01:54:46 +0000

    added the function to create li elements from the stored cities but with bug

--- script.js
+++ script.js
@@ -5,31 +5,32 @@ let storedCities = [];
 
 
 
-// function renderCities() {
-//     // Clear element and update todoCountSpan
-//     citiesList.innerHTML = "";
+function renderCities() {
+    // Clear element and update todoCountSpan
+    storedCities.innerHTML = "";
 
-//     // Render a new li for each city
-//     for (var i = 0; i < citiesList.length; i++) {
-//         // create a variable that stores the "current" todo
-//         var city = citiesList[i];
+    // Render a new li for each city
+    for (var i = 0; i < storedCities.length; i++) {
+        // create a variable that stores the "current" todo
+        var city = storedCities[i];
 
-//         // create a li element (list item) that we will append to our list later
-//         var li = document.createElement("li"); // <li></li>
+        // create a li element (list item) that we will append to our list later
+        var li = document.createElement("li"); // <li></li>
 
-//         // set the text of the list item to be the value of todo
-//         li.textContent = city;   // <li>Teach  HTML</li>
+        // set the text of the list item to be the value of todo
+        li.textContent = city;   // <li>Teach  HTML</li>
 
-//         // make this the last child of the todoList, which is the <ul> on our page
+        // make this the last child of the todoList, which is the <ul> on our page
 
-//         citiesList.appendChild(li);
-//     }
-// }
+        citiesList.appendChild(li);
+    }
+}
 
 
 function init() {
     var cityToStore = JSON.parse(localStorage.getItem(`city`));
-    // storedCities = cityToStore;
+    storedCities = cityToStore;
+    
     
     //   renderCities()
 }
@@ -46,7 +47,7 @@ let buttonsDiv = document.querySelector(".cityButtons")
 buttonsDiv.addEventListener("click", event => {
     event.preventDefault();
     let city = document.querySelector(".weather-search").value;
-    if (event.target.tagName === 'BUTTON') {
+    if (event.target.id == 'search-button') {
         console.log(city);
         
         fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=cba5837e4c2716904395e63c014ee26b")
@@ -68,12 +69,12 @@ buttonsDiv.addEventListener("click", event => {
             
         })
         
+        storedCities.push(city);
+        renderCities();
     }
-    storedCities.push(city);
     // cityInput.value = "";
 
-    storeCities();
-    // renderCities();
+    // storeCities();
     
 });
 
