f5aab69110e4e201aebf9a947538dacfacae8dfe
manolisgi <114882721+manolisgi@users.noreply.github.com>
2023-01-29 21:16:41 +0000

    added the first day's date to the card

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let citiesList = document.querySelector(".list-group");
 let cityInput = document.querySelector(".form-input");
 let storedCities = [];
+let firstDayTime = document.querySelector(".first-day-time");
+
 // let chosenCity = document.querySelector(".form-input").value;
 
 
@@ -15,14 +17,17 @@ function renderCities() {
         var city = storedCities[i];
 
         // create a li element (list item) that we will append to our list later
-        var li = document.createElement("li"); // <li></li>
+        var button = document.createElement("button"); // <li></li>
 
         // set the text of the list item to be the value of todo
-        li.textContent = city;   // <li>Teach  HTML</li>
+        button.textContent = city;   // <li>Teach  HTML</li>
+
+        // add class to the button
+        button.className = "buttonCity";
 
         // make this the last child of the todoList, which is the <ul> on our page
 
-        citiesList.appendChild(li);
+        citiesList.appendChild(button);
     }
 }
 
@@ -47,8 +52,35 @@ let buttonsDiv = document.querySelector(".cityButtons")
 buttonsDiv.addEventListener("click", event => {
     event.preventDefault();
     let city = document.querySelector(".weather-search").value;
+    let cityFromButton = document.querySelector(".buttonCity");
     if (event.target.id == 'search-button') {
         console.log(city);
+        
+        fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=cba5837e4c2716904395e63c014ee26b")
+        .then(response => response.json())
+        .then(citiesFound => {
+            let firstCity = citiesFound[0];
+            
+            console.log(firstCity.lat);
+            console.log(firstCity.lon);
+            
+            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=cba5837e4c2716904395e63c014ee26b`)
+        })
+        
+
+        .then(response => response.json())
+        .then(data => {
+            
+            console.log(data);
+            firstDayTime.innerHTML = moment(data.list[0].dt, "X").format("DD/MM/YYYY");
+            
+        })
+        
+        storedCities.push(city);
+        renderCities();
+    }
+    else if (event.target.id == 'buttonCitySearch') {
+        console.log(cityFromButton);
         
         fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=cba5837e4c2716904395e63c014ee26b")
         .then(response => response.json())
@@ -69,7 +101,7 @@ buttonsDiv.addEventListener("click", event => {
             
         })
         
-        storedCities.push(city);
+        // storedCities.push(city);
         renderCities();
     }
     // cityInput.value = "";
